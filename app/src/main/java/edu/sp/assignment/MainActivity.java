79a14a02ca4326ff7e73b1274ddbeaaf7777fc84
eugenechee <56992752+eugenechee@users.noreply.github.com>
2020-01-28 16:56:47 +0800

    Initial commit

---
+++ app/src/main/java/edu/sp/assignment/MainActivity.java
@@ -0,0 +1,51 @@
+package edu.sp.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        ed1 = (EditText)findViewById(R.id.editText);
+        ed2 = (EditText)findViewById(R.id.editText2);
+
+        b2 = (Button)findViewById(R.id.button2);
+        tx1 = (TextView)findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().equals("admin") &&
+                        ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong
+                            Credentials",Toast.LENGTH_SHORT).show();
+
+                            tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
